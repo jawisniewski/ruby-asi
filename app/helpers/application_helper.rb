bb1cafccf85cc133aee100f06cf9e55b314c6eb1
atejszerska <aax847@gmail.com>
2017-03-16 16:29:16 +0100

    redcarpet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  
+  	def markdown(text)
+
+    	markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(hard_wrap: true))
+    	markdown.render(text).html_safe
+
+	end
+
 end
